2d42280653771c466611b91677ce3420f4978152
Patricia Takehara <paty.takeharab@outlook.com>
2022-12-15 14:26:47 -0300

    Commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Dictionary<string, string> estados = new Dictionary<string, string>();
+
+estados.Add("SP", "São Paulo");
+estados.Add("RJ", "Rio de Janeiro");
+estados.Add("BA", "Bahia");
+estados.Add("MG", "Minas Gerais");
+
+foreach (var item in estados)
+{
+  Console.WriteLine($"CHave: {item.Key} Valor:{item.Value}");
+}
